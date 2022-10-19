2dcaa761623e6288aa0f4174586c97a9108830d9
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-10-19 06:29:20 +0200

    Hardcoded values in  Product Service - green

--- src/app/libs/products/services/products.service.ts
+++ src/app/libs/products/services/products.service.ts
@@ -3,7 +3,35 @@ import { ProductModel } from '../models/product.model';
 
 @Injectable()
 export class ProductsService {
-  getProducts() {
-
+  getProducts(): ProductModel[] {
+    return [{
+      id: '__PRODUCT_1_ID__',
+      title: '__PRODUCT_1_TITLE__',
+      price: {
+        amount: 123.23,
+        currency: 'PLN',
+      },
+      description: '__PRODUCT_1_DESCRIPTION__',
+      category: '__PRODUCT_1_CATEGORY__',
+      image: '__PRODUCT_1_IMAGE__',
+      rating: {
+        rate: 5,
+        count: 567
+      }
+    }, {
+      id: '__PRODUCT_2_ID__',
+      title: '__PRODUCT_2_TITLE__',
+      price: {
+        amount: 456.33,
+        currency: 'PLN',
+      },
+      description: '__PRODUCT_2_DESCRIPTION__',
+      category: '__PRODUCT_2_CATEGORY__',
+      image: '__PRODUCT_2_IMAGE__',
+      rating: {
+        rate: 4,
+        count: 987
+      }
+    }];
   }
 }
