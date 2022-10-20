2d64a8152cf5e7076d89b647ec3a7e66a9c575b7
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-10-20 22:36:25 +0200

    Products Container spec - added service in spec - red

--- src/app/libs/products/components/containers/products-list/products.component.spec.ts
+++ src/app/libs/products/components/containers/products-list/products.component.spec.ts
@@ -1,14 +1,26 @@
 import { ProductsComponent } from './products.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { ProductsService } from '../../../services/products.service';
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
   let fixture: ComponentFixture<ProductsComponent>;
+  let productsService: SpyObj<ProductsService>;
 
   beforeEach(() => {
+    productsService = createSpyObj('ProductsService', ['getProducts']);
+
     TestBed.configureTestingModule({
       declarations: [ ProductsComponent ],
+      providers: [
+        {
+          provide: ProductsService,
+          useValue: productsService
+        }
+      ]
     });
     fixture = TestBed.createComponent(ProductsComponent);
     component = fixture.componentInstance;
