5bb994f53eaa6ed8b243895c2888ccf0c43e2380
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-10-20 22:57:42 +0200

    Products Container spec - added service in spec - green

--- src/app/libs/products/components/containers/products-list/products.component.spec.ts
+++ src/app/libs/products/components/containers/products-list/products.component.spec.ts
@@ -4,6 +4,7 @@ import { By } from '@angular/platform-browser';
 import { ProductsService } from '../../../services/products.service';
 import SpyObj = jasmine.SpyObj;
 import createSpyObj = jasmine.createSpyObj;
+import { of } from 'rxjs';
 
 describe('ProductsComponent', () => {
   let component: ProductsComponent;
@@ -12,6 +13,37 @@ describe('ProductsComponent', () => {
 
   beforeEach(() => {
     productsService = createSpyObj('ProductsService', ['getProducts']);
+    productsService.getProducts.and.returnValue(of([
+      {
+        id: '__PRODUCT_ID_1__',
+        title: '__PRODUCT_TITLE_1__',
+        price: {
+          amount: 123.56,
+          currency: 'PLN',
+        },
+        description: '__PRODUCT_DESCRIPTION_1__',
+        category: '__PRODUCT_CATEGORY_1__',
+        image: '__PRODUCT_IMAGE_1__',
+        rating: {
+          rate: 5,
+          count: 234
+        }
+      }, {
+        id: '__PRODUCT_ID_2__',
+        title: '__PRODUCT_TITLE_2__',
+        price: {
+          amount: 67.89,
+          currency: 'USD',
+        },
+        description: '__PRODUCT_DESCRIPTION_2__',
+        category: '__PRODUCT_CATEGORY_2__',
+        image: '__PRODUCT_IMAGE_2__',
+        rating: {
+          rate: 4,
+          count: 56
+        }
+      }
+    ]));
 
     TestBed.configureTestingModule({
       declarations: [ ProductsComponent ],
--- src/app/libs/products/components/containers/products-list/products.component.ts
+++ src/app/libs/products/components/containers/products-list/products.component.ts
@@ -1,5 +1,7 @@
 import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
 import { ProductsService } from '../../../services/products.service';
+import { ProductModel } from '../../../models/product.model';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-products-list',
@@ -9,5 +11,7 @@ import { ProductsService } from '../../../services/products.service';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ProductsComponent {
+  readonly products$: Observable<ProductModel[]> = this._productsService.getProducts();
+
   constructor(private _productsService: ProductsService) {}
 }
