23bd83068bda9a33bd6ac53071c0d4ad35fbba3d
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-10-20 18:33:08 +0200

    Switched to API call in Product Service - refactor IV

--- src/app/libs/products/services/products.service.spec.ts
+++ src/app/libs/products/services/products.service.spec.ts
@@ -2,9 +2,10 @@ import { TestBed } from '@angular/core/testing';
 import { ProductModel } from '../models/product.model';
 import { ProductsService } from './products.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { ProductResponse } from './product.response';
 
 describe('ProductsService', () => {
-  const  given = async () => {
+  const  given = async (response: ProductResponse[]) => {
     await TestBed.configureTestingModule({
       imports: [ HttpClientTestingModule ],
       providers: [
@@ -50,7 +51,30 @@ describe('ProductsService', () => {
   // });
 
   it('should return product list', async () => {
-    const { when, httpTestingController } = await given();
+    const response: ProductResponse[] = [{
+      id: '__PRODUCT_1_ID__',
+      title: '__PRODUCT_1_TITLE__',
+      price: 123.23,
+      description: '__PRODUCT_1_DESCRIPTION__',
+      category: '__PRODUCT_1_CATEGORY__',
+      image: '__PRODUCT_1_IMAGE__',
+      rating: {
+        rate: 5,
+        count: 567
+      }
+    }, {
+      id: '__PRODUCT_2_ID__',
+      title: '__PRODUCT_2_TITLE__',
+      price: 456.33,
+      description: '__PRODUCT_2_DESCRIPTION__',
+      category: '__PRODUCT_2_CATEGORY__',
+      image: '__PRODUCT_2_IMAGE__',
+      rating: {
+        rate: 4,
+        count: 987
+      }
+    }];
+    const { when, httpTestingController } = await given(response);
 
     const expectedProductData: ProductModel[] = [{
       id: '__PRODUCT_1_ID__',
@@ -89,29 +113,7 @@ describe('ProductsService', () => {
     const req = httpTestingController.expectOne('https://fakestoreapi.com/products');
     expect(req.request.method).toEqual('GET');
 
-    const response = [{
-      id: '__PRODUCT_1_ID__',
-      title: '__PRODUCT_1_TITLE__',
-      price: 123.23,
-      description: '__PRODUCT_1_DESCRIPTION__',
-      category: '__PRODUCT_1_CATEGORY__',
-      image: '__PRODUCT_1_IMAGE__',
-      rating: {
-        rate: 5,
-        count: 567
-      }
-    }, {
-      id: '__PRODUCT_2_ID__',
-      title: '__PRODUCT_2_TITLE__',
-      price: 456.33,
-      description: '__PRODUCT_2_DESCRIPTION__',
-      category: '__PRODUCT_2_CATEGORY__',
-      image: '__PRODUCT_2_IMAGE__',
-      rating: {
-        rate: 4,
-        count: 987
-      }
-    }];
+
     req.flush(response);
 
     httpTestingController.verify();
