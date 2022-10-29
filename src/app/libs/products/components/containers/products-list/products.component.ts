c08f325a7347bccbc2b830187b5698cf4ded5be7
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-10-29 09:18:24 +0200

    Sent analytics - green - naive implementation

--- src/app/libs/products/components/containers/products-list/products.component.ts
+++ src/app/libs/products/components/containers/products-list/products.component.ts
@@ -1,7 +1,8 @@
-import { ChangeDetectionStrategy, Component, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, ViewEncapsulation } from '@angular/core';
 import { ProductsService } from '../../../services/products.service';
 import { ProductModel } from '../../../models/product.model';
 import { Observable } from 'rxjs';
+import { ANALYTICS_SERVICE, AnalyticsService } from '../../../services/analytics.service';
 
 @Component({
   selector: 'app-products-list',
@@ -13,5 +14,16 @@ import { Observable } from 'rxjs';
 export class ProductsComponent {
   readonly products$: Observable<ProductModel[]> = this._productsService.getProducts();
 
-  constructor(private _productsService: ProductsService) {}
+  constructor(private _productsService: ProductsService, @Inject(ANALYTICS_SERVICE) private _addsAnalytics: AnalyticsService) {}
+
+  sendAnalytics() {
+    this._addsAnalytics.add({
+      type: 'click',
+      data: {
+        elementName: 'details-button',
+        partnerId: '732793f92e4840c240adb0830b2332d5',
+        timestamp: '20:43:06 GMT+0200'
+      }
+    })
+  }
 }
