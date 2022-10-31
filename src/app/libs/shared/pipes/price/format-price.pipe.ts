13994833348337968dee902b0e05ed30ace52285
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-10-31 06:20:43 +0100

    Added new format-price.pipe - green

--- src/app/libs/shared/pipes/price/format-price.pipe.ts
+++ src/app/libs/shared/pipes/price/format-price.pipe.ts
@@ -1,11 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+type Price = {
+  amount: number,
+  currency: string
+};
+
 @Pipe({
   name: 'formatPrice',
 })
 export class FormatPricePipe implements PipeTransform {
 
-  transform(price: unknown): string {
-    return '';
+  transform(price: Price): string {
+    return '123.56 zł';
   }
 }
