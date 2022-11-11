fc520bc980f8c58298d8e9999763263d618568e4
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-11-11 14:36:55 +0100

    Used new stars component in Product List

--- src/app/libs/products/components/containers/products-list/products.component-module.ts
+++ src/app/libs/products/components/containers/products-list/products.component-module.ts
@@ -5,9 +5,10 @@ import {
   SendAnalyticsDirectivesModule
 } from '../../../../shared/directives/send-analytics-on-click/send-analytics-directives.module';
 import { FormatPricePipeModule } from '../../../../shared/pipes/price/format-price-pipe.module';
+import { StarsComponentModule } from '../../../../shared/components/stars/stars.component-module';
 
 @NgModule({
-  imports: [CommonModule, SendAnalyticsDirectivesModule, FormatPricePipeModule],
+  imports: [CommonModule, SendAnalyticsDirectivesModule, FormatPricePipeModule, StarsComponentModule],
   declarations: [ProductsComponent],
   providers: [],
   exports: [ProductsComponent]
