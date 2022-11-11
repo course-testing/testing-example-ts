86d20cd264e17ed4498f3bd1b7e23174d99fd962
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-11-11 11:10:32 +0100

    e2e - Product list - refactor

--- cypress/e2e/products.cy.ts
+++ cypress/e2e/products.cy.ts
@@ -2,13 +2,23 @@ describe('Product list', () => {
   const given = () => {
     cy.visit('http://localhost:4200/products');
 
+    const productList = () => cy.get('[data-selector="product-container"]')
+
     return {
-      productList: () => cy.get('[data-selector="product-container"]'),
+      productList,
+      firstItemTitle: () => productList().first().find('[data-cy="product-title"]'),
     };
   };
 
   it('should display products list', () => {
     const { productList } = given();
     productList().should('have.length', 20)
+  })
+
+  it('should display product elements', () => {
+    const { firstItemTitle } = given();
+
+    firstItemTitle()
+      .should('exist');
   })
 })
