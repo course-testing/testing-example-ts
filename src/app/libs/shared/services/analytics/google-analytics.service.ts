4db7c07ff87e0d4d274d958afd2062359e718fa5
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-11-11 09:26:15 +0100

    Added GoogleAnalyticsService spec - green

--- src/app/libs/shared/services/analytics/google-analytics.service.ts
+++ src/app/libs/shared/services/analytics/google-analytics.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map } from 'rxjs';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsDto } from './analytics.dto';
 import { HttpClient } from '@angular/common/http';
@@ -10,6 +10,6 @@ export class GoogleAnalyticsService implements AnalyticsService {
   }
 
   add(analyticsDto: AnalyticsDto): Observable<void> {
-    return of(void 0);
+    return this._httpClient.post(`https://analytics.free.beeceptor.com/my/api/path`, analyticsDto).pipe(map(() => void 0));
   }
 }
