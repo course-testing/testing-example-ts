4aec79de89f7a06ce9c65c9b4df4c1d03b923b24
Tomasz Paloc <tomasz.paloc+testing@gmail.com>
2022-11-11 13:43:29 +0100

    Added stars.component.ts - green

--- src/app/libs/shared/components/stars/stars.component.ts
+++ src/app/libs/shared/components/stars/stars.component.ts
@@ -7,5 +7,16 @@ import { ChangeDetectionStrategy, Component, Input, ViewEncapsulation } from '@a
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StarsComponent {
-  @Input() rate: number = 0;
+  private readonly MAX_RATE = 5;
+
+  rateValue = 0;
+  filledStars = 0;
+  emptyStars = 5;
+
+  @Input()
+  set rate(rate: number) {
+    this.rateValue = Math.min(rate, this.MAX_RATE);
+    this.filledStars = Math.min(parseInt(String(this.rateValue)), this.MAX_RATE);
+    this.emptyStars = this.MAX_RATE - this.filledStars;
+  }
 }
